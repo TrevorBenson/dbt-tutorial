d2ebec15de007ebc713258cf44bff3f46c4a00c2
Trevor Benson <trevor.benson@scality.com>
2025-04-08 18:42:43 +0000

    Document models

--- jaffle_shop/models/staging/stg_customers.sql
+++ jaffle_shop/models/staging/stg_customers.sql
@@ -4,6 +4,13 @@
   )
 }}
 
+{% docs view_stg_customer %}
+
+This view contains a cleaned up version of the `jaffle_shop.customers` table.
+It renames the columns to be more user-friendly.
+
+{% enddocs %}
+
 select
     id as customer_id,
     first_name,
